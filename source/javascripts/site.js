f9487e9df1b747358d9bba502ed8fd73dac1f673
Josef Zacek <joezacek@gmail.com>
2019-10-21 12:57:26 +0100

    add leafletjs map location

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,15 @@
 // This is where it all goes :)
+
+// leaflet map on contact page
+  if ($('#contact-map').length) {
+    var map = L.map('contact-map').setView([49.261439, 17.702072], 14);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([49.261439, 17.702072]).addTo(map).bindPopup('Cukrářství Kostelec,<br>Zlínská 107,<br>Kostelec,<br>76314,<br>Zlín');
+
+    map.dragging.disable();
+    map.scrollWheelZoom.disable();
+  }
